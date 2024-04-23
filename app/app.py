878f6df4a9eb6d1cb90228447d3a0fe5a436c4a0
Sriharish Pingali <sriharish.pingali@gmail.com>
2024-04-22 20:52:22 -0700

    basic structure

--- app/app.py
+++ app/app.py
@@ -1,8 +1,50 @@
-from fastapi import FastAPI
+import httpx
+from typing import Annotated
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from models import endpoints
+
+class CompletionRequest(BaseModel):
+    model: str
+    messages: list
+
+class CompletionResponseTest(BaseModel):
+    model: str
+    messages: list
 
 app = FastAPI()
 
+"""
+Defining a POST endpoint /completions
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+Header and body should be structured exactly as it would be passed to OpenAI, Mistral, or Anthropic
+Header will be passed as given
+Request authentication is done automatically by FastAPI
+"""
+@app.post("/completions/")
+def completions(item: CompletionRequest, request: Request) -> CompletionResponseTest:
+    model = item.model
+    if model not in endpoints:
+        return BaseException
+
+    response = httpx.post(
+        endpoints[model], 
+        data={
+            "model": item.model,
+            "messages": item.messages,
+        },
+        headers=request.headers
+    )
+
+    return response
+
+@app.post("/test/")
+def test(item: CompletionRequest, request: Request) -> CompletionResponseTest:
+    print(request.headers)
+    return item
+
+"""
+OpenAI: Authorization: Bearer [token]
+Mistral: Authorization: Bearer [token]
+Claude: x-api-key: [token]
+"""
---
+++ app/models.py
@@ -0,0 +1,12 @@
+"""
+Dictionary of modelName-endpoint key-value pairs
+"""
+endpoints = {
+    "gpt-3.5-turbo":"https://api.openai.com/v1/chat/completions",
+    "gpt-4":"https://api.openai.com/v1/chat/completions",
+    "claude-2.1":"",
+    "open-mistral-7b":"https://api.mistral.ai/v1/models/chat/completions",
+    "mistral-small":"https://api.mistral.ai/v1/models/chat/completions", 
+    "mistral-medium":"https://api.mistral.ai/v1/models/chat/completions",
+    "test":"http://127.0.0.1:8000/test/"
+}
